6d6c79c632fcdca0725cdabf15876084bb38c324
magfur2309 <magfurgezo@gmail.com>
2025-02-25 10:39:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from datetime import datetime, timedelta
 from supabase import create_client
 
 # Inisialisasi Supabase
-SUPABASE_URL = "your_supabase_url"
-SUPABASE_KEY = "your_supabase_key"
+SUPABASE_URL = "https://ukajqoitsfsolloyewsj.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYWpxb2l0c2Zzb2xsb3lld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjUyMDEsImV4cCI6MjA1NTYwMTIwMX0.vllN8bcBG-wpjA9g7jjTMQ6_Xf-OgJdeIOu3by_cGP0"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def find_invoice_date(pdf_file):
