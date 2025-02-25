b32adba29c3cc7442f3c5dbfb6450c8eb09c1b8c
magfur2309 <magfurgezo@gmail.com>
2025-02-25 14:46:31 +0700

    Update login.py

--- login.py
+++ login.py
@@ -4,12 +4,15 @@ from supabase import create_client
 import bcrypt
 import os
 from dotenv import load_dotenv
-import streamlit as st
 
-# Load environment variables
-load_dotenv()
-st.write("https://ukajqoitsfsolloyewsj.supabase.co:", st.secrets.get("https://ukajqoitsfsolloyewsj.supabase.co"))
-st.write("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYWpxb2l0c2Zzb2xsb3lld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjUyMDEsImV4cCI6MjA1NTYwMTIwMX0.vllN8bcBG-wpjA9g7jjTMQ6_Xf-OgJdeIOu3by_cGP0:", st.secrets.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYWpxb2l0c2Zzb2xsb3lld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjUyMDEsImV4cCI6MjA1NTYwMTIwMX0.vllN8bcBG-wpjA9g7jjTMQ6_Xf-OgJdeIOu3by_cGP0"))
+# Ambil secrets dengan aman
+SUPABASE_URL = st.secrets["https://ukajqoitsfsolloyewsj.supabase.co"]
+SUPABASE_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYWpxb2l0c2Zzb2xsb3lld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjUyMDEsImV4cCI6MjA1NTYwMTIwMX0.vllN8bcBG-wpjA9g7jjTMQ6_Xf-OgJdeIOu3by_cGP0"]
+
+# Debugging untuk cek apakah nilai terbaca
+st.write("SUPABASE_URL:", https://ukajqoitsfsolloyewsj.supabase.co)
+st.write("SUPABASE_KEY:", eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrYWpxb2l0c2Zzb2xsb3lld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjUyMDEsImV4cCI6MjA1NTYwMTIwMX0.vllN8bcBG-wpjA9g7jjTMQ6_Xf-OgJdeIOu3by_cGP0)
+
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def hash_password(password):
